1ec4c2faf1e8e01da605b7afbc7fbf1a3e2efb80
Kazuki Yoshida <idkphys.030zzz@gmail.com>
2019-08-21 23:22:15 +0900

    feat: add get Post API store

--- src/store/post.ts
+++ src/store/post.ts
@@ -1,37 +1,75 @@
 import Vue from 'vue'
 import axios from 'axios'
+import _ from 'lodash'
 import { API } from '../constants/api'
+import { IPost } from '../interfaces/post'
 import { IPostSummary } from '../interfaces/posts'
 
 export interface IState {
   posts: IPostSummary[] | null
+  post: Record<string, IPost>
 }
 
 export const state = (): IState => ({
-  posts: null
+  posts: null,
+  post: {}
 })
 
 export const getters = {
   getPosts(state: IState): IPostSummary[] | null {
     return state.posts
+  },
+  getPost: (state: IState): any => (
+    id: number
+  ): IPost | null => {
+    return state.post[String(id)]
   }
 }
 
 export const mutations = {
   savePosts(state: IState, posts: IPostSummary[]): void {
     state.posts = posts
+  },
+  savePost(
+    state: IState,
+    payload: { id: number, post: IPost }
+  ): void {
+    const { id, post } = payload
+    Vue.set(state.post, String(id), post)
   }
 }
 
 export const actions = {
   async fetchPosts(
     this: Vue,
-    { state, commit }: any,
-    payload: {}
+    { state, commit }: any
   ): Promise<void> {
     console.log('>> fetchPosts')
-    const { posts } = await axios.get(API.POSTS)
-    commit('savePosts', posts)
-    // WIP: APIリクエストの完了/未完了を判定するためには try-catch する必要あり
-  }
+
+    // キャッシュがあれば早期リターン
+    if (state.posts) return
+
+    console.log("get request : ", API.POSTS)
+
+    const { data } = await axios.get(API.POSTS)
+    commit('savePosts', data.posts)
+  },
+
+  async fetchPost(
+    this: Vue,
+    { state, commit }: any,
+    id: number
+  ): Promise<void> {
+    console.log('>> fetchPost id: ', id)
+
+    // キャッシュがあれば早期リターン
+    if (_.keys(state.post).indexOf(String(id)) >= 0) return
+
+    console.log("get request : ", API.POST + String(id))
+
+    const { data } = await axios.get(API.POST + String(id))
+    commit('savePost', { id: id, post: data })
+  },
+
+
 }
