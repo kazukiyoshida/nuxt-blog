dc3a99a81e00985eb8aa9988bd6e76c589e15513
Kazuki Yoshida <idkphys.030zzz@gmail.com>
2019-08-22 00:01:19 +0900

    feat: change favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ const config: NuxtConfiguration = {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/png', href: '/favicon.png' }
     ]
   },
   srcDir: 'src/',
