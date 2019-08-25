aa73665ceb49daf3a0c2009e037597fb8134bd22
Kazuki Yoshida <idkphys.030zzz@gmail.com>
2019-08-25 23:51:42 +0900

    chore: change website name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ const config: NuxtConfiguration = {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: "Kazuki's Blog",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
