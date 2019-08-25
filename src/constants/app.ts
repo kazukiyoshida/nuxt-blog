36fbcf70ae41c40bcbd38f01fa1b46e5c9d79231
Kazuki Yoshida <idkphys.030zzz@gmail.com>
2019-08-26 01:55:32 +0900

    feat: add PAGE constant

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -9,3 +9,11 @@ export enum COLOR {
   BLACK = 'black',
   WHITE = 'white'
 }
+
+/** リンク */
+export const LINK = {
+  TWITTER: "https://twitter.com/_kazukiyoshida_",
+  GITHUB: "https://github.com/kazukiyoshida",
+  INSTAGRAM: "https://www.instagram.com/kazukiyoshida_tokyo/",
+  GOOGLEMAPS: "https://goo.gl/maps/HsYYAXrmg2Yma6jP9"
+}
