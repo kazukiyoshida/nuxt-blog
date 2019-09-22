6ddee12825171dae5a232c1ecb213a65b9fe0aab
Kazuki Yoshida <idkphys.030zzz@gmail.com>
2019-09-23 01:24:42 +0900

    feat: k8s, ingress, ssl

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,5 +1,7 @@
 /** API のエンドポイント */
 export const API = {
-  POSTS: 'http://localhost:3000/api/posts',
-  POST: 'http://localhost:3000/api/posts/',
+  // POSTS: 'http://localhost:3000/api/posts',
+  // POST: 'http://localhost:3000/api/posts/',
+  POSTS: 'http://34.102.217.227/api//posts',
+  POST: 'http://34.102.217.227/api/posts/',
 }
